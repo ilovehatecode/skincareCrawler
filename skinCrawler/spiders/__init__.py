88ca7f61e387f4444e453ceb4f3f2fc71a882e15
Michael Walker <woufon07@gmail.com>
2019-07-04 16:19:46 -0400

    gets ingredients list from product page

--- skinCrawler/spiders/__init__.py
+++ skinCrawler/spiders/__init__.py
@@ -11,30 +11,31 @@ import scrapy
 class SkincareSpider(scrapy.Spider):
     name = "skincare"
     start_urls = urls = [
-            'https://www.amazon.com/s?rh=n%3A3760911%2Cn%3A%2111055981%2Cn%3A11060451%2Cn%3A11060711&s=review-rank&qid=1562212393&ref=lp_11060711_st'
+            'https://www.amazon.com/s?i=beauty&rh=n%3A3760911%2Cn%3A11055981%2Cn%3A11060451%2Cn%3A11060711&s=featured-rank&qid=1562260218&ref=sr_st_featured-rank'
         ]
 
     def parse(self, response):
-        product_link_list = response.css('.s-line-clamp-4 a::attr(href)').getall()
+ ##       product_link_list = response.css('.s-line-clamp-4 a::attr(href)').getall()
+        product_link_list = response.css('.a-link-normal.a-text-normal::attr(href)').getall()
         for product_link in product_link_list:
             if product_link is not None:
 ##                yield {
 ##                 "product_link" : product_link
                 yield response.follow(product_link, callback=self.parseProductPage)
 ##             }
-               
         
     def parseProductPage(self, response):
-        ingred_list = response.css('.content:nth-child(3) p::text').get()
-        
-        self.log("Ingredient List: %s" % ingred_list)
-        if ingred_list is not None:
-            yield
-            {
-                'ingredient_list' : ingred_list
-            }
-        else:
-            yield
-            {
-                'response' : None
-            }
+        important_content = response.css('#importantInformation .content').get()
+        if important_content is not None:
+            startIndex = important_content.find('Ingredients')
+            startIndex = important_content.find('>', startIndex) + 1
+            endIndex = important_content.find('<', startIndex)
+            ingred_list = important_content[startIndex:endIndex]
+            self.log("Ingredient List: %s" % ingred_list)
+            if ingred_list is not None:
+                self.log("Running this part")
+                yield
+                {
+                    'ingredient_list' : ingred_list
+                }
+            
