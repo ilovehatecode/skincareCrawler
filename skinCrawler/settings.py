c0d00f4d605e9e5d415c75fa3e8f86e1b59bf9e5
Michael Walker <woufon07@gmail.com>
2019-07-04 13:27:11 -0400

    crawls first page of amazon skincare products

--- skinCrawler/settings.py
+++ skinCrawler/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'skinCrawler.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'skinCrawler (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- skinCrawler/spiders/__init__.py
+++ skinCrawler/spiders/__init__.py
@@ -2,3 +2,39 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+#
+# Crawl Amazon.com for skincare products
+
+import scrapy
+
+
+class SkincareSpider(scrapy.Spider):
+    name = "skincare"
+    start_urls = urls = [
+            'https://www.amazon.com/s?rh=n%3A3760911%2Cn%3A%2111055981%2Cn%3A11060451%2Cn%3A11060711&s=review-rank&qid=1562212393&ref=lp_11060711_st'
+        ]
+
+    def parse(self, response):
+        product_link_list = response.css('.s-line-clamp-4 a::attr(href)').getall()
+        for product_link in product_link_list:
+            if product_link is not None:
+##                yield {
+##                 "product_link" : product_link
+                yield response.follow(product_link, callback=self.parseProductPage)
+##             }
+               
+        
+    def parseProductPage(self, response):
+        ingred_list = response.css('.content:nth-child(3) p::text').get()
+        
+        self.log("Ingredient List: %s" % ingred_list)
+        if ingred_list is not None:
+            yield
+            {
+                'ingredient_list' : ingred_list
+            }
+        else:
+            yield
+            {
+                'response' : None
+            }
